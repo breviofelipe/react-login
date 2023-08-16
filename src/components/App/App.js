177d0885d793128009226015b4abb84131280778
Felipe Brevio <felipebrevio05@gmail.com>
2023-08-15 21:06:08 -0300

    componente HackerLogin

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,11 @@ import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import LoginPage from '../Login/LoginPage';
 import LoginHacker from '../Login/HackerLogin';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { Routes ,Route, Switch } from 'react-router-dom';
 
 function Login() {
   return (
     <div className="wrapper">
-      <LoginPage />
       <LoginHacker />
       <BrowserRouter>
         <Switch>
